67af2b05197ef03e5f9b26ddbd254674975aef35
srmarshall0 <srmarshall@pbs.org>
2025-03-20 21:41:43 -0400

    text offset on hover

---
+++ portfolio-site/src/lib/NavigationMenu.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+</script>
+
+<div class="navigation-menu">
+	<a class="nav-menu-page-title" href="/about">
+		<img class="nav-menu-about" src="/svgs/about.svg" alt="ABOUT" />
+	</a>
+	<a class="nav-menu-page-title" href="/projects">
+		<img class="nav-menu-projects" src="/svgs/projects.svg" alt="PROJECTS" />
+	</a>
+	<a class="nav-menu-page-title" href="/contact">
+		<img class="nav-menu-contact" src="/svgs/contact.svg" alt="CONTACT" />
+	</a>
+</div>
+
+<style>
+	.navigation-menu {
+		display: flex;
+		flex-direction: column;
+		align-items: flex-end;
+		font-size: 64px;
+	}
+	.nav-menu-page-title {
+		color: var(--secondary-purple);
+	}
+
+	.nav-menu-about:hover {
+		margin-top: -10px; /* TODO: find a better way to get this to work */
+		content: url('/svgs/about-hover.svg');
+	}
+
+	.nav-menu-projects:hover {
+		content: url('/svgs/projects-hover.svg');
+		margin-top: -10px; /* TODO: find a better way to get this to work */
+	}
+
+	.nav-menu-contact:hover {
+		content: url('/svgs/contact-hover.svg');
+		margin-top: -10px; /* TODO: find a better way to get this to work */
+	}
+
+	a {
+		text-decoration: none;
+	}
+</style>
