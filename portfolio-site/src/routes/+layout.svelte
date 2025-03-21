0518d4cb14387a1184ad64b433c2f5d2ff5e67a1
srmarshall0 <srmarshall@pbs.org>
2025-03-20 20:51:38 -0400

    first commit

---
+++ portfolio-site/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../global.css';
+
+	let { children } = $props();
+</script>
+
+<nav>
+	<a href="/">HOME</a>
+	<a href="/about">ABOUT</a>
+</nav>
+
+{@render children()}
