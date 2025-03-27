d56867a40d137680e987d851b07cccae29bc85e7
sophie-marshall <sophie.marshall98@gmail.com>
2025-03-26 22:43:20 -0400

    about, basic static chat

--- portfolio-site/src/routes/about/+page.svelte
+++ portfolio-site/src/routes/about/+page.svelte
@@ -1,1 +1,25 @@
-<h1>ABOUT</h1>
+<script lang="ts">
+    import AboutInformation from "$lib/AboutInformation.svelte";
+    import Chat from "$lib/Chat.svelte";
+</script>
+<main>
+    <AboutInformation />
+    <div class="chat-container">
+        <Chat />
+    </div>
+</main>
+
+<style>
+    main {
+        display: grid;
+        grid-template-columns: 40% 60%;
+        height: 100%;
+        gap: 30px;
+    }
+
+    .chat-container {
+        height: 80%;
+        align-self: flex-end;
+    }
+    
+</style>
