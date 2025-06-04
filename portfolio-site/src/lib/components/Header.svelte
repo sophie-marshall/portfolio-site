97de75fdf97dc4705af5493d0a3d44dd3d72d04f
sophie-marshall <sophie.marshall98@gmail.com>
2025-06-04 11:55:35 -0400

    foundations

--- portfolio-site/src/lib/components/Header.svelte
+++ portfolio-site/src/lib/components/Header.svelte
@@ -12,6 +12,9 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
+		position: sticky;
+		top: 0;
+		z-index: 99;
 	}
 
 	.header img {
--- portfolio-site/src/lib/components/Project.svelte
+++ portfolio-site/src/lib/components/Project.svelte
@@ -1,13 +1,47 @@
 <script lang="ts">
-	let { projectName } = $props();
+	let { projectName, thumbnail } = $props<{
+		projectName: string;
+		thumbnail?: string;
+	}>();
 </script>
 
 <div class="project">
-	This is a project named: {projectName}
+	<div class="thumbnail-container">
+		<img class="thumbnail" src={thumbnail} alt="Project thumbnail" />
+		<div class="overlay">
+			<h2>{projectName}</h2>
+		</div>
+	</div>
 </div>
 
 <style>
-	.project {
-		background: pink;
+	.thumbnail-container {
+		aspect-ratio: 4/3;
+		overflow: hidden;
+		position: relative;
+	}
+
+	.thumbnail {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		object-position: center;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0, 0, 0, 0.5);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		opacity: 0;
+	}
+
+	.thumbnail-container:hover .overlay {
+		opacity: 1;
 	}
 </style>
