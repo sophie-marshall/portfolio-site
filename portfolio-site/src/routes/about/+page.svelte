0ffe1e6da2ef408abbb87a4a202911fec828d900
Sophie Marshall <sophie.marshall98@gmail.com>
2025-06-04 18:57:03 -0400

    aesthetics (#23)

--- portfolio-site/src/routes/about/+page.svelte
+++ portfolio-site/src/routes/about/+page.svelte
@@ -4,26 +4,30 @@
 <main>
 	<div class="text-content">
 		<div class="overview">
-			<span>Howdy</span> -- Sophie here! I am a full-time <span>Data Engineer</span>, part-time
-			triathlete, and unofficial energy drink connoisseur. I live in Washington DC with my partner
-			and our long-haired chihuahua, Penelope!
+			<span class="span-color-text">Howdy</span> -- Sophie here! I am a full-time
+			<span class="span-color-text">Data Engineer</span>, part-time triathlete, and unofficial
+			energy drink connoisseur. I live in Washington DC with my partner and our long-haired
+			chihuahua, Penelope!
 		</div>
 		<div class="professional-summary">
 			I'm most passionate about building pipelines that keep emerging technology systems hydrated,
 			but I'm always up for exploring new frontiers. I have a strong foundation in <span
-				>Python</span
+				class="span-color-text">Python</span
 			>,
-			<span>SQL</span> and <span>JavaScript</span>, with plans to expand to Java and Rust as the
-			data landscape continues to evolve.
+			<span class="span-color-text">SQL</span> and <span class="span-color-text">JavaScript</span>,
+			with plans to expand to Java and Rust as the data landscape continues to evolve.
 		</div>
 
 		<div class="current-position">
-			Right now, I work on a <span>R&D</span> team focused on developing and championing
-			<span>ML/AI</span> based workflows in public media.
+			Right now, I work on a <span class="span-color-text">R&D</span> team focused on developing and
+			championing
+			<span class="span-color-text">ML/AI</span> based workflows in public media.
 		</div>
 
 		<div class="contact">
-			<a href="mailto:sophie.marshall98@gmail.com"><span>sophie.marshall98@gmail.com</span></a>
+			<a href="mailto:sophie.marshall98@gmail.com"
+				><span class="span-color-text">sophie.marshall98@gmail.com</span></a
+			>
 		</div>
 	</div>
 	<img src="/images/profile-photo.JPG" alt="Me and some french onion dip!" />
