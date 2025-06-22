a20e8abca3d7c147ca4be23c68baeac50ae99558
sophie-marshall <sophie.marshall98@gmail.com>
2025-06-22 18:33:10 -0400

    mayube an idea?

--- portfolio-site/src/routes/blog/+page.svelte
+++ portfolio-site/src/routes/blog/+page.svelte
@@ -1,1 +1,42 @@
-<main>Hello from the Blog!</main>
+<main class="blog-page">
+	<div class="timeline">
+		<div class="top">
+			<div class="post">This is a blog post</div>
+			<div class="post">This is a blog post</div>
+		</div>
+		<div class="bottom">
+			<div class="post">This is a blog post</div>
+			<div class="post">This is a blog post</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	.timeline {
+		display: flex;
+		flex-direction: column;
+	}
+
+	.top,
+	.bottom {
+		display: flex;
+		gap: 300px;
+	}
+
+	.top {
+		background: lightcoral;
+		border-bottom: 4px solid white;
+	}
+
+	.bottom {
+		margin-left: 300px;
+		background: lightseagreen;
+	}
+
+	.post {
+		display: flex;
+		height: 200px;
+		width: 300px;
+		border: 0.5px solid black;
+	}
+</style>
