6167005d2dba8a9e96ea320460d29bd1b29eb66a
sophie-marshall <sophie.marshall98@gmail.com>
2025-09-11 13:49:23 -0400

    update links on about

--- portfolio-site/src/routes/about/+page.svelte
+++ portfolio-site/src/routes/about/+page.svelte
@@ -4,24 +4,28 @@
 <main>
 	<div class="text-content">
 		<div class="overview">
-			<span class="span-color-text">Howdy</span> -- Sophie here! I am a full-time
-			<span class="span-color-text">Data Engineer</span>, part-time triathlete, and unofficial
-			energy drink connoisseur. I live in Washington DC with my partner and our long-haired
-			chihuahua, Penelope!
+			<a href="https://medium.com/@sophie.marshall98/allow-me-to-introduce-myself-682afdd1e520"
+				><span class="span-color-text">Sophie here!</span></a
+			>
+			I am a full-time
+			<a href="https://github.com/sophie-marshall"
+				><span class="span-color-text">Data Engineer</span></a
+			>, part-time
+			<a href="https://strava.app.link/bDRlds21zWb"
+				><span class="span-color-text">triathelte</span></a
+			>, and unofficial energy drink connoisseur. I live in Washington DC with my partner and our
+			long-haired chihuahua, Penelope!
 		</div>
 		<div class="professional-summary">
 			I'm most passionate about building pipelines that keep emerging technology systems hydrated,
-			but I'm always up for exploring new frontiers. I have a strong foundation in <span
-				class="span-color-text">Python</span
-			>,
-			<span class="span-color-text">SQL</span> and <span class="span-color-text">JavaScript</span>,
-			with plans to expand to Java and Rust as the data landscape continues to evolve.
+			but I'm always up for a new challence. I have a strong foundation in Python, SQL and
+			JavaScript, with plans to expand to Java and Rust as the data landscape continues to evolve.
 		</div>
 
 		<div class="current-position">
-			Right now, I work on a <span class="span-color-text">R&D</span> team focused on developing and
-			championing
-			<span class="span-color-text">ML/AI</span> based workflows in public media.
+			Right now, I work on a <a href="https://innovation.pbs.org/"
+				><span class="span-color-text">R&D team</span></a
+			> focused on developing and championing ML/AI based workflows in public media.
 		</div>
 
 		<div class="contact">
